4efc99d06f79664312a3b461dfcd946d55e85f84
Danny Armijos <daniel2667@hotmail.es>
2025-04-03 22:12:47 +0200

    Agregar soporte para DatePipe en BasicPage y mejorar la presentación de fechas

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,35 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 import { TitlePipeComponent } from '../../components/title-pipe/title-pipe.component';
 
 @Component({
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, TitlePipeComponent],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    TitlePipeComponent,
+    DatePipe,
+  ],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal('Danny');
   nameUpper = signal('DANNY');
   fullName = signal('DAnNY Armijos');
+
+  customDate = signal<Date>(new Date());
+  tickindDate = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
 }
