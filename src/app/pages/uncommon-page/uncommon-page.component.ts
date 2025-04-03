41709d86c174d215a278408dd912187dfad9f96b
Danny Armijos <daniel2667@hotmail.es>
2025-04-04 00:27:38 +0200

    Agregar soporte para i18nPluralPipe en UncommonPage y funcionalidad para borrar clientes

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { TitlePipeComponent } from '../../components/title-pipe/title-pipe.component';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'Danny',
@@ -18,7 +18,7 @@ const client2 = {
 };
 
 @Component({
-  imports: [TitlePipeComponent, CardComponent, I18nSelectPipe],
+  imports: [TitlePipeComponent, CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -26,9 +26,9 @@ export default class UncommonPageComponent {
   cliente = signal(client1);
 
   invitationMap = {
-    male:'invitarlo',
-    female: 'invitarla'
-  }
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
   changeClient() {
     if (this.cliente() === client1) {
@@ -37,5 +37,32 @@ export default class UncommonPageComponent {
     }
 
     this.cliente.set(client1);
+  }
+
+  clientesMap =  signal({
+    '=0':'no tenemos ningún cliente esperando',
+    '=1':'tenemos un cliente esperando',
+    '=2':'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  })
+  // i18n Plural
+  clientes = signal<string[]>([
+    'Ana',
+    'Juan',
+    'Elena',
+    'Carlos',
+    'Sofía',
+    'Mateo',
+    'Lucía',
+    'Daniel',
+    'Paula',
+    'Alejandro',
+  ]);
+
+  deleteClient() {
+    this.clientes.update((prev) => {
+      prev.pop();
+      return prev;
+    });
   }
 }
