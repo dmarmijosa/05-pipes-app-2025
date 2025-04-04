0a64863f5c39c8db467d6f859f02042d31fe4d02
Danny Armijos <daniel2667@hotmail.es>
2025-04-04 13:19:06 +0200

    Agregar soporte para múltiples pipes personalizados en CustomPage, incluyendo ToggleCasePipe, CanFlyPipe y HeroColorPipe, y mejorar la presentación de datos.

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TitlePipeComponent } from "../../components/title-pipe/title-pipe.component";
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/canFly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
-  imports: [TitlePipeComponent],
+  imports: [TitlePipeComponent, ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name= signal('angular');
+  toggle = signal(false);
+
+  changeToggle(){
+    this.toggle.set(!this.toggle());
+  }
+
+  heroes =  signal(heroes)
+}
